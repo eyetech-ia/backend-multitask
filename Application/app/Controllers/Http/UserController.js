661203fcb1821eb4c53fcf83552e294ea0ca7646
Victor Mesquita <mesquitadev@gmail.com>
2021-03-31 14:46:05 -0300

    M - add Docker

---
+++ Application/app/Controllers/Http/UserController.js
@@ -0,0 +1,88 @@
+'use strict'
+const User = use('App/Models/User')
+const { uuid } = require('uuidv4')
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with users
+ */
+class UserController {
+  /**
+   * Show a list of all users.
+   * GET users
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index ({ request, response, view }) {
+    await User.all()
+  }
+
+  /**
+   * Create/save a new user.
+   * POST users
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response }) {
+    const data = request.only(['name', 'cpf', 'cargo', 'locale_id', 'birthdate', 'email', 'password'])
+    data.has_login = false
+    data.register_token = uuid()
+    const user = await User.create(data)
+    return user
+  }
+
+  /**
+   * Display a single user.
+   * GET users/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params, request, response, view }) {
+  }
+
+  /**
+   * Render a form to update an existing user.
+   * GET users/:id/edit
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async edit ({ params, request, response, view }) {
+  }
+
+  /**
+   * Update user details.
+   * PUT or PATCH users/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request, response }) {
+  }
+
+  /**
+   * Delete a user with id.
+   * DELETE users/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, request, response }) {
+  }
+}
+
+module.exports = UserController
---
+++ Application/start/routes.js
@@ -0,0 +1,38 @@
+'use strict'
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URL's and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+const Env = use('Env')
+
+Route.get('/', ({ response }) => {
+  return response.redirect('/api/v1')
+})
+Route.group(() => {
+  Route.get('/', () => {
+    return {
+      success: `Server running on ${Env.get('HOST')} in port: ${Env.get('PORT')}`
+    }
+  })
+
+  Route.post('login', 'SessionController.store')
+  Route.resource('users', 'UserController').apiOnly().middleware('auth')
+  Route.resource('locale', 'LocaleController').apiOnly().middleware('auth')
+  Route.resource('quest', 'QuestController').apiOnly().middleware('auth')
+  Route.resource('ask', 'AskController').apiOnly().middleware('auth')
+  Route.resource('permissions', 'PermissionController').apiOnly().middleware('auth')
+  Route.resource('roles', 'RoleController').apiOnly().middleware('auth')
+  // Route.post('visitante/validate', 'Ambient//ValidateController.store')
+  // Route.get('visitante/validate/:token', 'Ambient//ValidateController.index')
+}).prefix('api/v1')
